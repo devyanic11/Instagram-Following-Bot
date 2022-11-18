e1644c74d7bb753d1dd1001edacbb8df29dcd209
devyanic11 <104618068+devyanic11@users.noreply.github.com>
2022-11-18 22:38:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 import time
 
-USERNAME = "lambda_lily"
-PASSWORD = "pythontext"
+USERNAME = "YOUR USERNAME"
+PASSWORD = "YOUR PASSWORD"
 
-chrome_driver_path ="E:/Courses/100 Days of Code The Complete Python Pro Bootcamp for 2022/My Projects/chromedriver_win32/chromedriver.exe"
+chrome_driver_path = "YOUR CHROMEDRIVER PATH"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://www.instagram.com/jannatzubair29/")
+driver.get("https://www.instagram.com/wonderful_lives/")
 
 log_in = WebDriverWait(driver, timeout=60).until(lambda d: d.find_element(By.CSS_SELECTOR, 'a button'))
 log_in.click()
@@ -23,18 +23,23 @@ password.send_keys(PASSWORD)
 time.sleep(2)
 enter = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button')
 enter.click()
+
 time.sleep(15)
 not_now = WebDriverWait(driver, timeout=30).until(lambda d: d.find_element(By.CSS_SELECTOR, '._ac8f button'))
 not_now.click()
 
+#To click on Followers List
 time.sleep(10)
 followers = WebDriverWait(driver, timeout=60).until(lambda d: d.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[2]/div[2]/section/main/div/header/section/ul/li[2]/a'))
 followers.click()
 
-buttons = WebDriverWait(driver, timeout=60).until(lambda d: d.find_elements(By.NAME, 'Follow'))
+#To Click on Follow Button in Popup
+buttons = WebDriverWait(driver, timeout=60).until(lambda d: d.find_elements(By.TAG_NAME, 'button'))
 for btn in buttons:
     driver.execute_script("arguments[0].click();", btn)
     time.sleep(2)
+    
+#To Scroll in Popup
 # scr1 = WebDriverWait(driver, timeout=60).until(lambda d: d.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]'))
 # while True:
 #     driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scr1)
